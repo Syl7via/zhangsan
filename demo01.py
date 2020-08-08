488cb64b84d4c4b43d67b59106a00ca26a0f26a1
Sylvia <2080697363@qq.com>
2020-08-08 14:59:46 +0800

    第一次向GitHub提交代码

---
+++ demo01.py
@@ -0,0 +1,25 @@
+"""
+print("hello")   #字符串
+
+哈哈哈
+
+
+
+print("haha",2333)
+a="张三"
+print(a)
+
+"""
+
+a=(input ("请输入："))
+b=(input ("请输入："))
+
+print("input获取的内容： ",len(a)+len(b))
+
+
+
+
+
+
+# a=int("2333")
+# print(type(a))
